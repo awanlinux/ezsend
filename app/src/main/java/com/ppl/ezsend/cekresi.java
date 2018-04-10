9980f3540f2defba9159e425c66adf0f029cac57
bedolnyagru@gmail.com <asuscorei0@gmail.com>
2018-04-11 02:01:06 +0700

    update abdul 1

---
+++ app/src/main/java/com/ppl/ezsend/cekresi.java
@@ -0,0 +1,14 @@
+package com.ppl.ezsend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cekresi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cekresi);
+        this.setTitle( "Track Paket");
+    }
+}
