5b54880662b938ad1df29fb1b213b00a46de7130
a4tz <fuadfakhrizal.gmail.com>
2018-04-11 01:05:58 +0700

    update fuad 1

---
+++ app/src/main/java/com/ppl/ezsend/MainActivity.java
@@ -0,0 +1,99 @@
+package com.ppl.ezsend;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton MyImageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ntit);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+//        final Animation animRotate = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
+
+        MyImageButton = (ImageButton) findViewById(R.id.user);
+        MyImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+//                V.startAnimation(animRotate);
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, kirim.class);
+
+                startActivity(intentLoadNewActivity);
+            }
+
+        });
+
+        MyImageButton = (ImageButton) findViewById(R.id.job);
+        MyImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, cekresi.class);
+
+                startActivity(intentLoadNewActivity);
+            }
+
+        });
+
+        MyImageButton = (ImageButton) findViewById(R.id.notif);
+        MyImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+
+                Intent intentLoadNewActivity = new Intent(MainActivity.this,cekharga.class);
+
+                startActivity(intentLoadNewActivity);
+            }
+
+        });MyImageButton = (ImageButton) findViewById(R.id.mail);
+        MyImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, droppoint2.class);
+
+                startActivity(intentLoadNewActivity);
+            }
+
+        });
+
+        MyImageButton = (ImageButton) findViewById(R.id.rate);
+        MyImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, contactus.class);
+
+                startActivity(intentLoadNewActivity);
+            }
+
+        });
+
+        MyImageButton = (ImageButton) findViewById(R.id.rev);
+        MyImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, aboutus.class);
+
+                startActivity(intentLoadNewActivity);
+            }
+
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/ppl/ezsend/hubungi_kami.java
@@ -0,0 +1,13 @@
+package com.ppl.ezsend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class hubungi_kami extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hubungi_kami);
+    }
+}
